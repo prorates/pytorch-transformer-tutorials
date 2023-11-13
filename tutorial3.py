73396450173a4d635a9aaa7da58e601cc465a55a
Jerome Brette <jbrette@gmail.com>
2023-11-12 19:05:49 -0600

    move special scheduler back to tutorial3

--- tutorial3.py
+++ tutorial3.py
@@ -15,6 +15,77 @@ from model3 import Transformer3, build_transformer3
 from utils import collect_training_metrics, reload_model, save_model, load_trained_model
 
 
+class CosineWithRestarts(torch.optim.lr_scheduler._LRScheduler):
+    """
+    Cosine annealing with restarts.
+
+    Parameters
+    ----------
+    optimizer : torch.optim.Optimizer
+
+    T_max : int
+        The maximum number of iterations within the first cycle.
+
+    eta_min : float, optional (default: 0)
+        The minimum learning rate.
+
+    last_epoch : int, optional (default: -1)
+        The index of the last epoch.
+
+    """
+
+    def __init__(self,
+                 optimizer: torch.optim.Optimizer,
+                 T_max: int,
+                 eta_min: float = 0.,
+                 last_epoch: int = -1,
+                 factor: float = 1.) -> None:
+        # pylint: disable=invalid-name
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.factor = factor
+        self._last_restart: int = 0
+        self._cycle_counter: int = 0
+        self._cycle_factor: float = 1.
+        self._updated_cycle_len: int = T_max
+        self._initialized: bool = False
+        super(CosineWithRestarts, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """Get updated learning rate."""
+        # HACK: We need to check if this is the first time get_lr() was called, since
+        # we want to start with step = 0, but _LRScheduler calls get_lr with
+        # last_epoch + 1 when initialized.
+        if not self._initialized:
+            self._initialized = True
+            return self.base_lrs
+
+        step = self.last_epoch + 1
+        self._cycle_counter = step - self._last_restart
+
+        lrs = [
+            (
+                self.eta_min + ((lr - self.eta_min) / 2) *
+                (
+                    np.cos(
+                        np.pi *
+                        ((self._cycle_counter) % self._updated_cycle_len) /
+                        self._updated_cycle_len
+                    ) + 1
+                )
+            ) for lr in self.base_lrs
+        ]
+
+        if self._cycle_counter % self._updated_cycle_len == 0:
+            # Adjust the cycle length.
+            self._cycle_factor *= self.factor
+            self._cycle_counter = 0
+            self._updated_cycle_len = int(self._cycle_factor * self.T_max)
+            self._last_restart = step
+
+        return lrs
+
+
 def build_model3(config: dict, vocab_src_len: int, vocab_tgt_len: int) -> Transformer3:
     model = build_transformer3(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'],
                                d_model=config['d_model'], n_layers=config['N'], heads=config['h'], dropout=config['dropout'])
@@ -73,8 +144,8 @@ def train_model3(config: dict):
     model = build_model3(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], betas=(0.9, 0.98), eps=1e-9)
-    # if opt.SGDR == True:
-    #     opt.sched = CosineWithRestarts(opt.optimizer, T_max=opt.train_len)
+    if False:
+        scheduler = CosineWithRestarts(opt.optimizer, T_max=opt.train_len)
 
     # Tensorboard
     writer = SummaryWriter(get_model_folder(config) + "/" + config['experiment_name'])
@@ -169,6 +240,7 @@ def translate3(config: dict, sentence: str):
     # if the sentence is a number use it as an index to the test set
     # run_translation(label, sentence, model, tokenizer_src, tokenizer_tgt, config['seq_len'], device)
 
+
 def debug_code_model3(config: dict, device):
     config['model'] = "model3"
     config['datasource'] = "translate"
@@ -185,10 +257,9 @@ def debug_code_model3(config: dict, device):
     print(model)
     model.train()
 
+
 if __name__ == '__main__':
     # warnings.filterwarnings('ignore')
     config = get_config()
     device = get_device()
     debug_code_model3(config, device)
-
-
